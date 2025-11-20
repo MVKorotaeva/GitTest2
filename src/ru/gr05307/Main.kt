cb49965ed240a8166092de036b684941582617a6
Smak2025 <smak-80+2025@yandex.ru>
2025-11-20 11:17:24 +0300

    Smak's Changes

--- src/ru/gr05307/Main.kt
+++ src/ru/gr05307/Main.kt
@@ -1,5 +1,11 @@
 package ru.gr05307
 
+import ru.gr05307.model.SmakClass
+
 fun main() {
     println("Hi!")
+    val sc = SmakClass(3, 5)
+    println(sc)
+    sc.b = 8
+    println(sc)
 }
---
+++ src/ru/gr05307/model/SmakClass.kt
@@ -0,0 +1,9 @@
+package ru.gr05307.model
+
+class SmakClass(
+    var a: Int,
+    var b: Int,
+) {
+
+    override fun toString() = "$a + $b"
+}
